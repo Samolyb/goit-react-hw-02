d501c2f38957c0174a9092581727e6831d9ee400
Samolyb <artemte6575@gmail.com>
2024-06-22 21:48:22 +0100

    React-hm-2

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,13 @@
+
+const Feedback = ({ feedback, totalFeedback, positiveFeedbackPercentage }) => (
+    <div>
+        <h2>Feedback Statistics</h2>
+        <p>Good: {feedback.good}</p>
+        <p>Neutral: {feedback.neutral}</p>
+        <p>Bad: {feedback.bad}</p>
+        <p>Total: {totalFeedback}</p>
+        <p>Positive feedback: {positiveFeedbackPercentage}%</p>
+    </div>
+);
+
+export default Feedback;
